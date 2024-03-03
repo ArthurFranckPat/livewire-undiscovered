5d8a9c64780d24b834f6c4fee392bcac1e4209cb
ArthurFranckPat <50202146+ArthurFranckPat@users.noreply.github.com>
2024-03-03 22:16:16 +0100

    episode 4 : passing data to javascript (snapshot)

--- app/Livewire.php
+++ app/Livewire.php
@@ -11,7 +11,18 @@ class Livewire
     function initialRender($class): string
     {
         $component = new $class;
-        return Blade::render($component->render(), $this->getProperties($component));
+        $snapshot = [
+            'class' => get_class($component),
+            'data' => $this->getProperties($component)
+        ];
+        $html = Blade::render($component->render(), $this->getProperties($component));
+        $snapshotAttribute = htmlentities(json_encode($snapshot));
+        return <<<HTML
+            <div wire:snapshot="{$snapshotAttribute}">
+            {$html}
+        </div>
+        HTML;
+
     }
     function getProperties($component): array
     {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,4 @@
-@php use App\Http\Livewire\Counter;use App\Livewire;use Illuminate\Support\Facades\Blade; @endphp
+s@php use App\Http\Livewire\Counter;use App\Livewire;use Illuminate\Support\Facades\Blade; @endphp
     <!DOCTYPE html>
 <html lang="en">
 <script src="https://cdn.tailwindcss.com"></script>
@@ -8,6 +8,13 @@
     @livewire(Counter::class)
 </body>
 
+<script>
+    // find all elements with wire:snapshot as attribute
+    const d = document.querySelectorAll("[wire\\:snapshot]").forEach(el => {
+        let snapshot = JSON.parse(el.getAttribute('wire:snapshot'))
+    console.log(snapshot)
+    })
+</script>
 
 </html>
 
