b3156d46b4a17ac347124fc84d6b7822ffe4ed36
ArthurFranckPat <50202146+ArthurFranckPat@users.noreply.github.com>
2024-03-03 21:55:01 +0100

    episode 3 : Livewire class + Blade directive

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Livewire;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::directive('livewire',function ($expression){
+            //dd($expression);
+
+/*            return "<?= (new Livewire())->initialRender($expression) ?>";*/
+            return "{!! (new Livewire())->initialRender({$expression}) !!}";
+        });
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,27 +1,13 @@
-@php use Illuminate\Support\Facades\Blade; @endphp
+@php use App\Http\Livewire\Counter;use App\Livewire;use Illuminate\Support\Facades\Blade; @endphp
     <!DOCTYPE html>
 <html lang="en">
 <script src="https://cdn.tailwindcss.com"></script>
-<body class="grid place-items-center min-h-screen bg-gray-100">
-{!! livewire(\App\Http\Livewire\Counter::class) !!}
+
+
+<body class="grid place-items-center h-screen bg-gray-100">
+    @livewire(Counter::class)
 </body>
 
 
 </html>
-
-<?php
-function livewire($class)
-{
-    $component = new $class;
-    return Blade::render($component->render(), getProperties($component));
-};
-function getProperties($component): array
-{
-    $properties = [];
-    $reflectedProperties = (new ReflectionClass($component))->getProperties(ReflectionProperty::IS_PUBLIC);
-    foreach ($reflectedProperties as $property) {
-        $properties[$property->getName()] = $property->getValue($component);
-    };
-    return $properties;
-}
 
